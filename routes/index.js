2756ceb0237792162ff4b1e67dd1aed036629458
Vanshaj Girotra <vansh@Vanshajs-MacBook-Pro.local>
2019-05-29 14:52:20 +0530

    upi form validation and validation form

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,63 @@ var router = express.Router();
 //console.log("index.js");
 /* GET home page. */
 
+
 router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
 router.get('/second' , function(req , res , next ){
-  console.log("get request at second");
-  console.log(req.query.phone);
-  console.log(req.query.email);
+ // console.log("get request at second");
+  //console.log(req.query.phone);
+  //console.log(req.query.email);
   res.render('second' , {cost : req.query.cost , email : req.query.email , phone : req.query.phone} );
 });
 
 router.post('/upiVerify' , function(req , res , next){
   var upiId = req.body.upiId;
-  console.log(upiId);
-  if (upiId === "vanshaj@bank")
+  //console.log(upiId);
+  const valid = validateUpi(upiId);
+  if (valid)
     res.send("true");
   else res.send("false");
 });
 
 /// post method not working 
 router.post('/second' , function (req , res , next){
-  console.log("post request at second");
-  console.log(req.body);
+ // console.log("post request at second");
+  //console.log(req.body);
   res.render('second' , {});
 });
 
+router.post('/third' , function(req , res , next){
+  console.log("in post third");
+  console.log(req.body);
+  res.render('third' , {});
+});
+router.get('/third' , function(req, res , next){
+  res.render('third');
+});
 module.exports = router;
+
+
+function validateUpi(upiId){
+  //alert("aaaaa");
+  let ret = false , pivot = false;
+  let left  = [] , right = [];
+  for (var i = 0;i < upiId.length; i++){
+      if (upiId[i] == "@"){
+          pivot =true;
+          continue;
+      }
+      if (!pivot)
+        left.push(upiId[i]);
+      else
+        right.push(upiId[i]);
+  }
+  //console.log(left);
+  //console.log(right);
+  //console.log(pivot);
+  if (left.length > 0  && right.length > 0 && pivot)
+    return true;
+  return false;
+}
